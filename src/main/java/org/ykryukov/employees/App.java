67f1770bf097bea54d239936d10b7c0f53427a25
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2024-02-15 18:01:47 +0600

    modified report printEmployeesGroupedByDOB

--- src/main/java/org/ykryukov/employees/App.java
+++ src/main/java/org/ykryukov/employees/App.java
@@ -1,20 +1,21 @@
 package org.ykryukov.employees;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class App {
     private static Set<Employee<Integer>> getEmployees() {
         Set<Employee<Integer>> employeeSet = new HashSet<>();
         Generable gen = new Generator();
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 50000; i++) {
             Employee<Integer> employee = new Employee<Integer>(
                     gen.generateInt(),
                     gen.generateString(10),
                     gen.generateString(10),
                     gen.generateString(10),
                     gen.generateDate(),
-                    gen.generateString(10),
-                    gen.generateString(10),
+                    gen.generateString(3),
+                    gen.generateString(3),
                     gen.generateString(25),
                     gen.generateString(15),
                     gen.generateDate(),
@@ -30,6 +31,19 @@ public class App {
         Set<Employee<Integer>> employeeSet = getEmployees();
 
         Report.printEmployeesGroupedByDOB(employeeSet);
+        System.out.println("---------------------------------------------------");
+        Report.printEmployeesGroupedByCountryAndCityResidence(employeeSet);
+
+//        List<String> list = new ArrayList<>();
+//        list.add("1|first");
+//        list.add("2|second");
+//        list.add("2|third");
+//        list.add("4|fourth");
+
+//        Map<String, String> map = list.stream().collect(Collectors.toMap(el -> el.split("\\|")[0], el -> el.split("\\|")[1], (oldVal, newVal) -> oldVal + ", " + newVal, HashMap::new));
+//        System.out.println(map);
+//
+//        System.out.println(list.stream().map(el -> el.split("\\|")[0]).collect(Collectors.toList()));
 
         //employee.setLastName("test1");
         //System.out.println(employee);
--- src/main/java/org/ykryukov/employees/Report.java
+++ src/main/java/org/ykryukov/employees/Report.java
@@ -5,18 +5,49 @@ import java.util.stream.Collectors;
 
 public class Report {
     public static void printEmployeesGroupedByDOB(Set<Employee<Integer>> employeeSet) {
+        int counter = 0;
         Map<Date, SortedSet<Employee<Integer>>> map = employeeSet.stream()
                 .collect(Collectors.groupingBy(Person::getDOB,
                         Collectors.mapping(emp -> emp, Collectors.toCollection(TreeSet::new))));
 
-        Map<Date, SortedSet<Employee<Integer>>> mapFiltered = map.entrySet().stream().filter(el->el.getValue().size() > 1)
+        Map<Date, SortedSet<Employee<Integer>>> mapFiltered = map.entrySet().stream().filter(el -> el.getValue().size() > 1)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
         System.out.println("EmployeesGroupedByDOB: ");
         for (Map.Entry<Date, SortedSet<Employee<Integer>>> entry : mapFiltered.entrySet()) {
+            if (counter > 10) {
+                break;
+            }
+            counter++;
             System.out.println("DOB: " + Util.dateToString(entry.getKey()));
             for (Employee<Integer> emp : entry.getValue()) {
                 System.out.println("Employee: " + emp);
+            }
+        }
+    }
+
+    public static void printEmployeesGroupedByCountryAndCityResidence(Set<Employee<Integer>> employeeSet) {
+        int counter = 0;
+//        Map<String, SortedSet<Employee<Integer>>> map = employeeSet.stream()
+//                .collect(Collectors.groupingBy(el -> el.getCountryOfResidence() + ", " + el.getCityOfResidence(),
+//                        Collectors.mapping(el -> el, Collectors.toCollection(TreeSet::new))));
+
+        SortedMap<String, SortedSet<Employee<Integer>>> map = employeeSet.stream()
+                .collect(Collectors.groupingBy(el -> el.getCountryOfResidence() + ", " + el.getCityOfResidence(), TreeMap::new,
+                        Collectors.mapping(el -> el, Collectors.toCollection(TreeSet::new))));
+
+        SortedMap<String, SortedSet<Employee<Integer>>> mapFiltered = map.entrySet().stream().filter(el -> el.getValue().size() > 1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldVal, newVal) -> oldVal, TreeMap::new));
+
+        System.out.println("printEmployeesGroupedByCountryAndCityResidence: ");
+        for (Map.Entry<String, SortedSet<Employee<Integer>>> entry : mapFiltered.entrySet()) {
+            if (counter > 10) {
+                break;
+            }
+            counter++;
+            System.out.println("Country, city: " + entry.getKey());
+            for (Employee<Integer> emp : entry.getValue()) {
+                System.out.println(emp);
             }
         }
     }
